c931cff37923f17481e7bb8999a2afc676e51baf
Shitalydv <167268556+Shitalydv@users.noreply.github.com>
2024-05-26 09:06:41 -0700

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,8 @@
+import random
+
+def random_words():
+    words = ['yohooo', 'apple', 'mango', 'robort', 'beans','artificialintelligence', 'batchthirtysix', 'classroom', 'ram', "rickandmorty", 
+       'helloworld','banana','deathnote','pineapple','anime','series','regenerating']
+    random_word = random.choice(words)
+
+    return random_word
